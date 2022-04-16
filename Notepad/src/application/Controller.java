e7814d4bcece4017fb3fae2c019497f20b353769
Samarthya Patel <86912122+SamarthyaPatel@users.noreply.github.com>
2022-04-16 23:33:41 +0100

    Updated File menu and Font
    Coded new, open, save, and font-related functions (bold and italic).

--- Notepad/src/application/Controller.java
+++ Notepad/src/application/Controller.java
@@ -1,9 +1,13 @@
 package application;
 
 import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -17,24 +21,34 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Menu;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class Controller implements Initializable {
 	
-	String[] fontArray = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-	ObservableList<String> fonts = FXCollections.observableArrayList();
+	private Stage stage;
+	private int size = 16;
+	private String name = "Arial";
+	private boolean bold = false;
+	private boolean italic = false;
+	private FontWeight bSyntax;
+	private FontPosture iSyntax;
+	
+	private String[] fontArray = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+	private ObservableList<String> fonts = FXCollections.observableArrayList();
 	
 	@FXML
 	ComboBox<String> comboBox;
 	
-	Stage stage;
-	
 	@FXML
 	TextArea textArea;
-	int size = 16;
-	String name = "Arial";
+	
 	
 	@FXML
 	AnchorPane anchorPane;
@@ -42,19 +56,48 @@ public class Controller implements Initializable {
 	@FXML
 	Menu fileMenu;
 	
+	@FXML
+	ToggleButton boldToggle;
+	
+	@FXML
+	ToggleButton italicToggle;
+	
+	
 	public void newFile(ActionEvent e) {
-		System.out.println("Open new file.");
+		textArea.setText("");
 	}
 	
-	public void open(ActionEvent e) {
-		System.out.println("Open file.");
+	public void open(ActionEvent e) throws FileNotFoundException {
+		FileChooser choose = new FileChooser();
+		choose.setTitle("Open file");
+		choose.setInitialDirectory(new File("c:\\users\\"));
+		choose.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"));
+		File file = choose.showOpenDialog(stage);
+		
+		String text = "";
+		Scanner scan = new Scanner(file);
+		while(scan.hasNext()) {
+			text += scan.nextLine() + "\n";
+		}
+		scan.close();
+		textArea.setText(text);
+		
 	}
 	
-	public void save(ActionEvent e) {
-		System.out.println("Save the file.");
+	public void save(ActionEvent e) throws IOException {
+		FileChooser choose = new FileChooser();
+		choose.setTitle("Save file");
+		choose.setInitialDirectory(new File("c:\\users\\"));
+		choose.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"));
+		File file = choose.showSaveDialog(stage);
+		
+		FileWriter pen = new FileWriter(file.getPath());
+		pen.write(textArea.getText());
+		pen.close();
+		
 	}
 	
-	public void close(ActionEvent e) {
+	public void close(ActionEvent e) throws IOException {
 		
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Close");
@@ -70,20 +113,62 @@ public class Controller implements Initializable {
 	
 	public void zoomIn(ActionEvent e) throws IOException {
 		size += 1;
-		textArea.setFont(new Font(name, size));
+		
+		setToggleButtons();
+		
+		textArea.setFont(Font.font(name, bSyntax, iSyntax, size));
 		
 	}
 	
 	public void zoomOut(ActionEvent e) {
 		size -= 1;
-		textArea.setFont(new Font(name, size));
+		
+		setToggleButtons();
+		
+		textArea.setFont(Font.font(name, bSyntax, iSyntax, size));
 	}
 	
 	public void changeFont(ActionEvent e) {
 		name = comboBox.getValue();
-		textArea.setFont(new Font(name, size));
-	}
 
+		setToggleButtons();
+		
+		textArea.setFont(Font.font(name, bSyntax, iSyntax, size));
+	}
+	
+	public void boldText(ActionEvent e) {
+		
+		setToggleButtons();
+		
+		textArea.setFont(Font.font(name, bSyntax, iSyntax, size));
+		
+	}
+	
+	public void italicText(ActionEvent e) {
+		
+		setToggleButtons();
+		
+		textArea.setFont(Font.font(name, bSyntax, iSyntax, size));
+		
+	}
+	
+	public void setToggleButtons() {
+		
+		bold = boldToggle.isSelected();
+		italic = italicToggle.isSelected();
+		
+		if(bold == true) {
+			bSyntax = FontWeight.BOLD;
+		} else {
+			bSyntax = FontWeight.NORMAL;
+		}
+		if(italic == true) {
+			iSyntax = FontPosture.ITALIC;
+		} else {
+			iSyntax = FontPosture.REGULAR;
+		}
+	}
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
@@ -92,6 +177,7 @@ public class Controller implements Initializable {
 		for(String s : fontArray) {
 			fonts.add(s);
 		}
+		
 		comboBox.getItems().addAll(fonts);
 	}
 	
--- Notepad/src/application/Main.java
+++ Notepad/src/application/Main.java
@@ -1,14 +1,10 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.image.ImageView;
-
 
 public class Main extends Application {
 	
@@ -21,9 +17,6 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			
 			stage.setTitle("Notepad");
-			stage.setResizable(false);
-			//stage.setWidth(500);
-			//stage.setHeight(500);
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
